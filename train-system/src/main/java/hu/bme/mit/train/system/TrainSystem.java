92072c98535f1a1e7ef4749985f7e97de300ecb8
KB <tigerbence@gmail.com>
2018-02-26 07:08:49 -0800

    Edited by KB

--- train-system/src/main/java/hu/bme/mit/train/system/TrainSystem.java
+++ train-system/src/main/java/hu/bme/mit/train/system/TrainSystem.java
@@ -12,7 +12,7 @@ public class TrainSystem {
 	private TrainController controller = new TrainControllerImpl();
 	private TrainUser user = new TrainUserImpl(controller);
 	private TrainSensor sensor = new TrainSensorImpl(controller, user);
-	private String edit = new String = "edit";
+	private String edit = new String = "reedit"; //I was here
 
 	public TrainController getController() {
 		return controller;
