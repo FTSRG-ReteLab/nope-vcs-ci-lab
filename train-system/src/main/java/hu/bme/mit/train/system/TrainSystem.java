b97f45b44ef35a653429fdc82c189484e72608ea
stekist <stekler94@gmail.com>
2018-02-26 06:51:53 -0800

    Added new string Edit to TrainSystem.java

--- train-system/src/main/java/hu/bme/mit/train/system/TrainSystem.java
+++ train-system/src/main/java/hu/bme/mit/train/system/TrainSystem.java
@@ -12,6 +12,7 @@ public class TrainSystem {
 	private TrainController controller = new TrainControllerImpl();
 	private TrainUser user = new TrainUserImpl(controller);
 	private TrainSensor sensor = new TrainSensorImpl(controller, user);
+	private String edit = new String = "edit";
 
 	public TrainController getController() {
 		return controller;
