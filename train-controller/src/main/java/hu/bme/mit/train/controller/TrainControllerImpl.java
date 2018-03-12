a98d97ffd513c3218924879832477faa5c834d9e
stekist <stekler94@gmail.com>
2018-03-12 07:24:17 -0700

    Implement changing ref speed

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -1,22 +1,28 @@
 package hu.bme.mit.train.controller;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import hu.bme.mit.train.interfaces.TrainController;
 
 public class TrainControllerImpl implements TrainController {
 
 	private int step = 0;
 	private int referenceSpeed = 0;
-	private int speedLimit = 0;
+	private int speedLimit = 50;
+	private int reverseSpeedLimit = 0;
+	
+	private Timer timer = new Timer();
 
 	@Override
 	public void followSpeed() {
-		if (referenceSpeed < 0) {
-			referenceSpeed = 0;
+		if (referenceSpeed < reverseSpeedLimit) {
+			referenceSpeed = reverseSpeedLimit;
 		} else {
-		    if(referenceSpeed+step > 0) {
+		    if(referenceSpeed+step > reverseSpeedLimit) {
                 referenceSpeed += step;
             } else {
-		        referenceSpeed = 0;
+		        referenceSpeed = reverseSpeedLimit;
             }
 		}
 
@@ -34,16 +40,34 @@ public class TrainControllerImpl implements TrainController {
 		enforceSpeedLimit();
 		
 	}
+	
+	@Override
+	public void setReverseSpeedLimit(int reverseSpeedLimit) {
+		this.reverseSpeedLimit = reverseSpeedLimit;
+		enforceSpeedLimit();
+
+	}
 
 	private void enforceSpeedLimit() {
 		if (referenceSpeed > speedLimit) {
 			referenceSpeed = speedLimit;
 		}
+		else if (referenceSpeed < reverseSpeedLimit)
+			referenceSpeed = reverseSpeedLimit;
 	}
 
 	@Override
 	public void setJoystickPosition(int joystickPosition) {
-		this.step = joystickPosition;		
+		this.step = joystickPosition;
+		
+		timer.schedule(new TimerTask() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+					referenceSpeed += step;
+				}
+		}, 1000);
 	}
 
 }
